ebc40e0eb1c30bea7a7afaa482aad7bc52d040ae
MarcoCaponera <marco.caponera4@gmail.com>
2024-05-03 16:20:29 +0200

    project commit

---
+++ Source/CppPlatformer/CppPlatformerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CppPlatformerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPPLATFORMER_API ACppPlatformerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
